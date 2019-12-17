b8e52cdbd9057d44ca6befbe8c66e5b83bb47337
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-12-17 14:50:01 -0500

    all CRUD ops endpoints added

--- routes/account-router.js
+++ routes/account-router.js
@@ -15,6 +15,25 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    db('accounts').where('id', req.params.id)
+    .then(accounts => {
+        const account = accounts[0]
+        if (account) {
+            res.json(account)
+        } else {
+            res.status(404).json({
+                message: 'invalid account id'
+            })
+        }
+    }) .catch (error => {
+        console.log(error)
+        res.status(500).json({
+            message: 'unable to retrieve an account'
+        })
+    })
+})
+
 router.post('/', (req, res) => {
     const accountData = req.body
       db('accounts').insert(accountData)
@@ -22,10 +41,47 @@ router.post('/', (req, res) => {
             res.status(201).json({ newAccountId: ids[0]} )
         }) .catch(error => {
             console.log(error)
-          res.status(500).json({ message: 'Could not add the account'
+          res.status(500).json({ 
+              message: 'Could not add the account'
          });
         });
     })
 
+router.put('/:id', (req, res) => {
+    const changedAccount = req.body
+    db('accounts').where('id', req.params.id).update(changedAccount)
+    .then(count => {
+        if(count) {
+            res.json({ updated: count })
+        } else {
+            res.status(404).json({
+                message: 'Invalid account id'
+            })
+        }
+    }) .catch (error => {
+        res.status(500).json({
+            message: 'Unable to update an account'
+        })
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    db('accounts').where('id', req.params.id).del()
+    .then(count => {
+        if(count) {
+            res.json({
+                message: `the account ${req.params.id} is nuked!`
+            })
+        } else {
+            res.status(404).json({
+                message: 'Invalid account id!'
+            })
+        }
+    }) .catch(error => {
+        res.status(500).json({
+            message: `Unable to delete the account!!!`
+        })
+    })
+})
 
 module.exports = router;
